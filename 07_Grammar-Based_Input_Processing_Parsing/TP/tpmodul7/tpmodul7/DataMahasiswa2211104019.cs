fb1ec33b55393cab0674259949044aa18a004ba5
edgarnadhif <edgarndf345@gmail.com>
2025-03-19 18:48:38 -0700

    Menambahkan class datamahasiswa2211104019 dan filejson

---
+++ 07_Grammar-Based_Input_Processing_Parsing/TP/tpmodul7/tpmodul7/DataMahasiswa2211104019.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+public class DataMahasiswa2211104019
+{
+    public Nama nama { get; set; }
+    public string nim { get; set; }
+    public string fakultas { get; set; }
+    public class Nama
+    {
+        public string depan { get; set; }
+        public string belakang { get; set; }
+    }
+    public static void ReadJSON()
+    {
+        string filePath = "tp7_1_2211104019.json";
+        if (File.Exists(filePath))
+        {
+            string jsonData = File.ReadAllText(filePath);
+            DataMahasiswa2211104019 mahasiswa = JsonConvert.DeserializeObject<DataMahasiswa2211104019>(jsonData);
+            Console.WriteLine($"Nama {mahasiswa.nama.depan} {mahasiswa.nama.belakang} dengan nim {mahasiswa.nim} dari fakultas {mahasiswa.fakultas}");
+        }
+        else
+        {
+            Console.WriteLine("File JSON tidak ditemukan.");
+        }
+    }
+}
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        DataMahasiswa2211104019.ReadJSON();
+//    }
+//}
---
+++ 07_Grammar-Based_Input_Processing_Parsing/TP/tpmodul7/tpmodul7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataMahasiswa2211104019.ReadJSON();
+
+        }
+    }
+}
