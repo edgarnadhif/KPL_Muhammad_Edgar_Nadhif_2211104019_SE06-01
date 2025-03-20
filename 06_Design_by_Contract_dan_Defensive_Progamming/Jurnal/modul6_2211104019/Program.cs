67eeaeef9208765597473c4644618748992f0dfc
edgarnadhif <edgarndf345@gmail.com>
2025-03-19 19:26:06 -0700

    commit pertama

---
+++ 06_Design_by_Contract_dan_Defensive_Progamming/Jurnal/modul6_2211104019/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul6_2211104019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
