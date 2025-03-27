f6945505e46fd17c352ce320823ee9bc07a00744
edgarnadhif <edgarndf345@gmail.com>
2025-03-26 20:25:57 -0700

    “menyelesaikan program dengan runtime configuration”

---
+++ 08_Runtime_Configuration_dan_Internationalization/TP/tp_05/CovidConfig.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+public class CovidConfig
+{
+    private const string FilePath = "covid_config.json";
+
+    public string SatuanSuhu { get; set; }
+    public int BatasHariDemam { get; set; }
+    public string PesanDitolak { get; set; }
+    public string PesanDiterima { get; set; }
+
+    public CovidConfig()
+    {
+        SatuanSuhu = "celcius";
+        BatasHariDemam = 14;
+        PesanDitolak = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+        PesanDiterima = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
+    }
+
+    public void LoadConfig()
+    {
+        if (File.Exists(FilePath))
+        {
+            string jsonData = File.ReadAllText(FilePath);
+            var config = JsonConvert.DeserializeObject<CovidConfig>(jsonData);
+            SatuanSuhu = config.SatuanSuhu;
+            BatasHariDemam = config.BatasHariDemam;
+            PesanDitolak = config.PesanDitolak;
+            PesanDiterima = config.PesanDiterima;
+        }
+        else
+        {
+            SaveConfig(); 
+        }
+    }
+
+    public void SaveConfig()
+    {
+        string jsonData = JsonConvert.SerializeObject(this, Formatting.Indented);
+        File.WriteAllText(FilePath, jsonData);
+    }
+
+    public void UbahSatuan()
+    {
+        SatuanSuhu = (SatuanSuhu == "celcius") ? "fahrenheit" : "celcius";
+        SaveConfig();
+    }
+}
--- 08_Runtime_Configuration_dan_Internationalization/TP/tp_05/Program.cs
+++ 08_Runtime_Configuration_dan_Internationalization/TP/tp_05/Program.cs
@@ -1,15 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
 
-namespace tp_05
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        CovidConfig config = new CovidConfig();
+        config.LoadConfig();  
+
+        Console.Write($"Berapa suhu badan anda saat ini? Dalam nilai {config.SatuanSuhu}: ");
+        double suhu = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam? ");
+        int hariDemam = Convert.ToInt32(Console.ReadLine());
+
+        bool suhuNormal = (config.SatuanSuhu == "celcius" && suhu >= 36.5 && suhu <= 37.5) ||
+                          (config.SatuanSuhu == "fahrenheit" && suhu >= 97.7 && suhu <= 99.5);
+
+        bool hariDemamNormal = hariDemam < config.BatasHariDemam;
+
+        if (suhuNormal && hariDemamNormal)
         {
+            Console.WriteLine(config.PesanDiterima);
+        }
+        else
+        {
+            Console.WriteLine(config.PesanDitolak);
+        }
+
+        Console.Write("Apakah ingin mengubah satuan suhu? (y/n): ");
+        string ubah = Console.ReadLine().ToLower();
+        if (ubah == "y")
+        {
+            config.UbahSatuan();
+            config.SaveConfig();
+            Console.WriteLine($"Satuan suhu sekarang: {config.SatuanSuhu}");
         }
     }
 }
