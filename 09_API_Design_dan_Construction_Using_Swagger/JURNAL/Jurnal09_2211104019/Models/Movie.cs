e8a60acce1fc9236b8ba67e8cffc4d17227c7d3a
edgarnadhif <edgarndf345@gmail.com>
2025-04-21 00:10:59 +0700

    Add : Add folder jurnal modul 9 & report Jurnal module 9

---
+++ 09_API_Design_dan_Construction_Using_Swagger/JURNAL/Jurnal09_2211104019/Models/Movie.cs
@@ -0,0 +1,12 @@
+namespace MovieAPI.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public List<string> Stars { get; set; }
+        public string Description { get; set; }
+
+        public Movie() { }
+    }
+}
